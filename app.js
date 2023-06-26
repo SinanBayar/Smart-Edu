aa6d720c77466956d5cd488f0e8256b81e767df9
Sinan Bayar <sinanbayar88@gmail.com>
2023-06-26 18:09:36 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index', {
+  page_name: "index"});
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"});
 });
