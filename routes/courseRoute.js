e2b9df3f55dabe03bdae389fef2bed636529fec9
Sinan Bayar <sinanbayar88@gmail.com>
2023-06-27 15:33:56 +0300

    Lesson 06

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 router.route('/').post(courseController.createCourse); // http://localhost:3000/courses
-
+router.route('/').get(courseController.getAllCourses);
 
 module.exports = router;
