f63dec74096e77ca6deaf7c9e7e77d3793d8623e
Sinan Bayar <sinanbayar88@gmail.com>
2023-06-28 17:47:32 +0300

    Lesson 13

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,7 @@ const bcrypt = require('bcrypt');
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -25,7 +22,7 @@ exports.loginUser = async (req, res) => {
         if (same) {
           // USER SESSION
           req.session.userID = user._id;
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
         }
       });
     }
@@ -40,5 +37,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID }); // id'si, session'daki userID'ye eşit olan kullanıcıyı bul.
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser); // http://localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser); // http://localhost:3000/users/logout
+router.route('/dashboard').get(authController.getDashboardPage); // http://localhost:3000/users/dashboard
 
 module.exports = router;
