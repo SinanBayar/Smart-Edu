559ebc0b9991c9e0d103820cd53e0664fa6dc149
Sinan Bayar <sinanbayar88@gmail.com>
2023-06-29 14:59:49 +0300

    Lesson 15

--- models/User.js
+++ models/User.js
@@ -16,6 +16,11 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ["Student", "Teacher", "Admin"], // Alabileceği değerler.
+    default: 'Student',
+  },
 });
 
 UserSchema.pre('save', function (next) {
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
+const roleMiddleware = require('../middlewares/roleMiddleware');
 
-router.route('/').post(courseController.createCourse); // http://localhost:3000/courses
+router.route('/').post(roleMiddleware(["Teacher", "Admin"]), courseController.createCourse); // http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 
